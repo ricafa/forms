a48fc51c3c9b4e08ef48ccdd3364d7c4d7f434c7
Ricardo <ri_ca_fa@hotmail.com>
2018-11-13 21:29:19 -0200

    fim aula 75 e 76

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: 'Loiane',
+    email: 'loiane@gtoner.com'
+  }
+
+  onSubmit(form) {
+    console.log(form, form.value.email, form.value.nome);
+    console.log(this.usuario);
+    
+  }
 
   constructor() { }
 
